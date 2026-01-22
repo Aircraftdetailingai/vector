501c91c417b11ef95d2bac4621c15d926871ef8c
Aircraftdetailingai <sales@shinyjets.com>
2026-01-21 21:10:31 -0800

    Add root layout

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Vector - Aircraft Detailing Quotes',
+  description: 'Professional quoting software for aircraft detailers',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
